b58351296302d346a783db308e4331d3c9c96727
Andrew Guterman <andrew.guterman1@gmail.com>
2019-09-24 21:39:39 -0700

    first cubeadd .

--- 50_SDL_and_opengl_2/50_SDL_and_opengl_2.cpp
+++ 50_SDL_and_opengl_2/50_SDL_and_opengl_2.cpp
@@ -158,11 +158,49 @@ void render()
 	//Render quad
 	if( gRenderQuad )
 	{
-		glBegin( GL_QUADS );
-			glVertex2f( -0.5f, -0.5f );
-			glVertex2f( 0.5f, -0.5f );
-			glVertex2f( 0.5f, 0.5f );
-			glVertex2f( -0.5f, 0.5f );
+		// White side - BACK
+		glBegin(GL_POLYGON);
+		glColor3f(   1.0,  0.0, 1.0 );
+		glVertex3f(  0.5, -0.5, 0.5 );
+		glVertex3f(  0.5,  0.5, 0.5 );
+		glVertex3f( -0.5,  0.5, 0.5 );
+		glVertex3f( -0.5, -0.5, 0.5 );
+		glEnd();
+
+		// Purple side - RIGHT
+		glBegin(GL_POLYGON);
+		glColor3f(  1.0,  0.0,  1.0 );
+		glVertex3f( 0.5, -0.5, -0.5 );
+		glVertex3f( 0.5,  0.5, -0.5 );
+		glVertex3f( 0.5,  0.5,  0.5 );
+		glVertex3f( 0.5, -0.5,  0.5 );
+		glEnd();
+
+		// Green side - LEFT
+		glBegin(GL_POLYGON);
+		glColor3f(   0.0,  1.0,  0.0 );
+		glVertex3f( -0.5, -0.5,  0.5 );
+		glVertex3f( -0.5,  0.5,  0.5 );
+		glVertex3f( -0.5,  0.5, -0.5 );
+		glVertex3f( -0.5, -0.5, -0.5 );
+		glEnd();
+
+		// Blue side - TOP
+		glBegin(GL_POLYGON);
+		glColor3f(   0.0,  0.0,  1.0 );
+		glVertex3f(  0.5,  0.5,  0.5 );
+		glVertex3f(  0.5,  0.5, -0.5 );
+		glVertex3f( -0.5,  0.5, -0.5 );
+		glVertex3f( -0.5,  0.5,  0.5 );
+		glEnd();
+
+		// Red side - BOTTOM
+		glBegin(GL_POLYGON);
+		glColor3f(   1.0,  0.0,  0.0 );
+		glVertex3f(  0.5, -0.5, -0.5 );
+		glVertex3f(  0.5, -0.5,  0.5 );
+		glVertex3f( -0.5, -0.5,  0.5 );
+		glVertex3f( -0.5, -0.5, -0.5 );
 		glEnd();
 	}
 }
