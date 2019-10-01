901fa2711960d21f00dcbdc49819398ee0a9f2fd
Andrew Guterman <andrew.guterman1@gmail.com>
2019-09-30 23:14:05 -0700

    we have happy cubes with camera!

--- happy_cameras/main.cpp
+++ happy_cameras/main.cpp
@@ -3,11 +3,24 @@ and may not be redistributed without written permission.*/
 
 //Using SDL, SDL OpenGL, standard IO, and, strings
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
-#include <GL/glu.h>
+// #include <SDL2/SDL_opengl.h>
+// #include <GL/glu.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "includes/stb_image.h"
+
+#include "includes/glad/glad.h"
+// #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "includes/learnopengl/filesystem.h"
+#include "includes/learnopengl/shader_m.h"
+#include "includes/learnopengl/camera.h"
+
+#include <iostream>
 
 #include <stdio.h>
 #include <string>
@@ -19,6 +32,17 @@ const int SCREEN_HEIGHT = 480;
 GLfloat rotationX = 0.0f;
 GLfloat rotationY = 0.0f;
 
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCREEN_WIDTH / 2.0f;
+float lastY = SCREEN_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// time
+Uint64 NOW = SDL_GetPerformanceCounter();
+Uint64 LAST = 0;
+double deltaTime = 0;
+
 //Starts up SDL, creates window, and initializes OpenGL
 bool init();
 
@@ -26,7 +50,7 @@ bool init();
 bool initGL();
 
 //Input handler
-void handleKeys( unsigned char key, int x, int y );
+void handleKeys( unsigned char key );
 
 //Per frame update
 void update();
@@ -134,6 +158,15 @@ bool init()
 			}
 			else
 			{
+				// glad: load all OpenGL function pointers
+				// ---------------------------------------
+				// if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+				if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
+				{
+					std::cout << "Failed to initialize GLAD" << std::endl;
+					return -1;
+			    }
+
 				//Use Vsync
 				if( SDL_GL_SetSwapInterval( 1 ) < 0 )
 				{
@@ -169,7 +202,8 @@ bool initGL()
 	error = glGetError();
 	if( error != GL_NO_ERROR )
 	{
-		printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+		printf("oh no");
+		// printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
 		success = false;
 	}
 
@@ -181,7 +215,8 @@ bool initGL()
 	error = glGetError();
 	if( error != GL_NO_ERROR )
 	{
-		printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+		printf("oh no");
+		// printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
 		success = false;
 	}
 	
@@ -192,15 +227,38 @@ bool initGL()
 	error = glGetError();
 	if( error != GL_NO_ERROR )
 	{
-		printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
+		printf("oh no");
+		// printf( "Error initializing OpenGL! %s\n", gluErrorString( error ) );
 		success = false;
 	}
 	
 	return success;
 }
 
-void handleKeys( unsigned char key, int x, int y )
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(double xpos, double ypos)
 {
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void handleKeys( unsigned char key )
+{
+	// mouse_callback(x, y);
+
 	//Toggle quad
 	if( key == 'q' )
 	{
@@ -209,18 +267,22 @@ void handleKeys( unsigned char key, int x, int y )
 
 	if( key == 'w' )
     {
+		camera.ProcessKeyboard(FORWARD, deltaTime);
         rotationY -= 10.f;
     }
     else if( key == 's' )
     {
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
         rotationY += 10.f;
     }
     else if( key == 'a' )
     {
+		camera.ProcessKeyboard(LEFT, deltaTime);
         rotationX -= 10.f;
     }
     else if( key == 'd' )
     {
+		camera.ProcessKeyboard(RIGHT, deltaTime);
         rotationX += 10.f;
     }
 }
@@ -307,6 +369,8 @@ void close()
 
 int main( int argc, char* args[] )
 {
+	unsigned int VBO, VAO;
+
 	//Start up SDL and create window
 	if( !init() )
 	{
@@ -314,6 +378,143 @@ int main( int argc, char* args[] )
 	}
 	else
 	{
+		// configure global opengl state
+		// -----------------------------
+		glEnable(GL_DEPTH_TEST);
+		Shader ourShader("7.4.camera.vs", "7.4.camera.fs");
+		
+		// set up vertex data (and buffer(s)) and configure vertex attributes
+		// ------------------------------------------------------------------
+		float vertices[] = {
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		};
+		// world space positions of our cubes
+		glm::vec3 cubePositions[] = {
+			glm::vec3( 0.0f,  0.0f,  0.0f),
+			glm::vec3( 2.0f,  5.0f, -15.0f),
+			glm::vec3(-1.5f, -2.2f, -2.5f),
+			glm::vec3(-3.8f, -2.0f, -12.3f),
+			glm::vec3( 2.4f, -0.4f, -3.5f),
+			glm::vec3(-1.7f,  3.0f, -7.5f),
+			glm::vec3( 1.3f, -2.0f, -2.5f),
+			glm::vec3( 1.5f,  2.0f, -2.5f),
+			glm::vec3( 1.5f,  0.2f, -1.5f),
+			glm::vec3(-1.3f,  1.0f, -1.5f)
+		};
+
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		// position attribute
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		// texture coord attribute
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+
+		// load and create a texture 
+		// -------------------------
+		unsigned int texture1, texture2;
+		// texture 1
+		// ---------
+		glGenTextures(1, &texture1);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		// set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// load image, create texture and generate mipmaps
+		int width, height, nrChannels;
+		stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+		unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+		// texture 2
+		// ---------
+		glGenTextures(1, &texture2);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		// set the texture wrapping parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// load image, create texture and generate mipmaps
+		data = stbi_load(FileSystem::getPath("resources/textures/awesomeface.png").c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			// note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+
+		// tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+		// -------------------------------------------------------------------------------------------
+		ourShader.use();
+		ourShader.setInt("texture1", 0);
+		ourShader.setInt("texture2", 1);
+
 		//Main loop flag
 		bool quit = false;
 
@@ -337,14 +538,62 @@ int main( int argc, char* args[] )
 				//Handle keypress with current mouse position
 				else if( e.type == SDL_TEXTINPUT )
 				{
+					// int x = 0, y = 0;
+					// SDL_GetMouseState( &x, &y );
+					handleKeys( e.text.text[ 0 ]);
+				}
+				else if(e.type == SDL_MOUSEMOTION) {
 					int x = 0, y = 0;
 					SDL_GetMouseState( &x, &y );
-					handleKeys( e.text.text[ 0 ], x, y );
+					mouse_callback(x, y);
 				}
 			}
 
 			//Render quad
-			render();
+			// render();
+			// per-frame time logic
+			// --------------------
+			LAST = NOW;
+			NOW = SDL_GetPerformanceCounter();
+
+			deltaTime = (double)((NOW - LAST)*1000 / (double)SDL_GetPerformanceFrequency() );
+			deltaTime /= 1000;
+
+			// render
+			// ------
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+
+			// bind textures on corresponding texture units
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, texture1);
+			glActiveTexture(GL_TEXTURE1);
+			glBindTexture(GL_TEXTURE_2D, texture2);
+
+			// activate shader
+			ourShader.use();
+
+			// pass projection matrix to shader (note that in this case it could change every frame)
+			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+			ourShader.setMat4("projection", projection);
+
+			// camera/view transformation
+			glm::mat4 view = camera.GetViewMatrix();
+			ourShader.setMat4("view", view);
+
+			// render boxes
+			glBindVertexArray(VAO);
+			for (unsigned int i = 0; i < 10; i++)
+			{
+				// calculate the model matrix for each object and pass it to shader before drawing
+				glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+				model = glm::translate(model, cubePositions[i]);
+				// float angle = 20.0f * i;
+				// model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+				ourShader.setMat4("model", model);
+
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
 			
 			//Update screen
 			SDL_GL_SwapWindow( gWindow );
@@ -354,9 +603,13 @@ int main( int argc, char* args[] )
 		SDL_StopTextInput();
 	}
 
+	// optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
 	//Free resources and close SDL
 	close();
 
 	return 0;
 }
-
